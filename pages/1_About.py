407fd776fef60f54fcb50853115e4aa4793e7402
ahmadsameh8 <112205028+ahmadsameh8@users.noreply.github.com>
2023-07-10 12:07:56 +0300

    Add files via upload

---
+++ pages/1_About.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="About the search Agent", page_icon="🌍")
+
+st.markdown("# About Agent")
+st.write("## This agent Searches multiple Search Engines that includes: Search Duck Duck go, Google search, Serper Api, wikipedia, Brave ")
+st.write('## This search engine uses an open source Large Language Model Called Bloom which is available on hugging face')
+
